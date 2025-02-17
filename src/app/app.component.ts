23b1e269808df0b906ca0a79231453714a1bfa53
kqmirasol <kqmirasol@icloud.com>
2025-02-17 11:38:23 +0800

    designed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,29 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, FormsModule, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'fin_activity';
+  title = 'Template-form-app';
+  username: string = "";
+  email: string = "";
+  password: string = "";
+  confirmPassword: string = "";
+  phone: string = "";
+  submitted: boolean = false;
+  showPassword = false;
+  showConfirmPassword: boolean = false;
+
+
+
+  onSubmit() {
+    this.submitted = true;
+  }
 }
